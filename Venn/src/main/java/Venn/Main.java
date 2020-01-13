942e7149e7950c296756184c621e20ee1ca8bb55
gagarao-melvin-216469637 <melving@my.yorku.ca>
2020-01-13 12:16:00 -0500

    deleted line 1

--- Venn/src/main/java/Venn/Main.java
+++ Venn/src/main/java/Venn/Main.java
@@ -3,7 +3,6 @@ package Venn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Group 1");
 		System.out.println("Edit #1");
 		System.out.println("Edit in Lab #1");
 		System.out.println("Edit  2 in Lab #1");
