b6daf15064b6a31ad9ea5c2d26e34adf882e5f72
Hassan Mukhtar <hassan.muk@hotmail.com>
2020-01-20 11:45:21 -0500

    hello

--- Venn/src/main/java/Venn/Main.java
+++ Venn/src/main/java/Venn/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("Edit in Lab #1");
 		System.out.println("Edit  2 in Lab #1");
 		System.out.println("Edit 3 in Lab #1");
-		System.out.println("Edit 3 in Lab #2");
+		System.out.println("Edit 3 in Lab  #2");
 
 
 
