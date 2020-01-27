c363868b18c626c9656c630baf854f89cd2ae046
zhoudoris13 <zhoudoris13@gmail.com>
2020-01-27 11:29:40 -0500

    File chooser created

--- VennFx/src/application/Main.java
+++ VennFx/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -16,6 +17,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- VennFx/src/application/SampleController.java
+++ VennFx/src/application/SampleController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class SampleController {
 	@FXML
@@ -13,10 +15,18 @@ public class SampleController {
 	private TextField textField;
 	@FXML
 	private Label label;
-
+	@FXML
+	private Button file;
 	@FXML 
 	public void buttonClicked() {
 		information = textField.getText();
 		label.setText(information);
+	}
+	
+	@FXML
+	public void fileButtonClicked() {
+		FileChooser filechooser = new FileChooser();
+		filechooser.setTitle("Open source File");
+		filechooser.showOpenDialog(new Stage());
 	}
 }
