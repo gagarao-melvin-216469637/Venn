002c93945507a14e3d832cb9696eb27ec02f50cf
gagarao-melvin-216469637 <melving@my.yorku.ca>
2020-01-29 21:47:00 -0500

    Added some commentary

--- Venn/src/main/java/Venn/SketchTester.java
+++ Venn/src/main/java/Venn/SketchTester.java
@@ -6,17 +6,20 @@ import processing.core.PApplet;
 
 public class SketchTester extends PApplet{
 	
+	// Define some containers and temporary variables
 	ArrayList<Shape> shapes = new ArrayList<Shape>();
 	ArrayList<TextBox> textBoxes = new ArrayList<TextBox>();
 	Color ellipseTemp = Color.decode("#52de97");
 	Color red = Color.decode("#da2d2d");
 	Color green = Color.decode("#21bf73");
 
+	// This is run once at the start of the program
 	public void settings() {
 		size(900, 500);
 		shapes.add(new Ellipse(this, ellipseTemp, width/2, height/2, 50, 50));
 	}
 	
+	// This is continuous loop that runs until terminated
 	public void draw() {
 		
 		background(255);
@@ -38,6 +41,7 @@ public class SketchTester extends PApplet{
 		
 	}
 	
+	// Mouse and Keyboard Listeners
 	public void mousePressed() {
 	}
 	
@@ -45,6 +49,7 @@ public class SketchTester extends PApplet{
 		exit();
 	}
 	
+	// The main method is unimportant for Processing
 	public static void main(String[] args) {
 		String[] processingArgs = {"SketchTester"};
 		SketchTester sketchTester = new SketchTester();
