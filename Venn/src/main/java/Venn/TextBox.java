17f687041ca9a9b1613fcba8a3b32943178c7ddb
gagarao-melvin-216469637 <melving@my.yorku.ca>
2020-01-29 21:44:07 -0500

    I added Processing to my branch to toy around with features

---
+++ Venn/src/main/java/Venn/TextBox.java
@@ -0,0 +1,20 @@
+package Venn;
+import processing.core.PApplet;
+
+public class TextBox {
+	PApplet sketch;
+	float x, y;
+	String letters;
+	int fontSize;
+
+	public TextBox(PApplet sketch, float x, float y, String letters, int fontSize) {
+		super();
+		this.sketch = sketch;
+		this.x = x;
+		this.y = y;
+		this.letters = letters;
+		this.fontSize = fontSize;
+	}
+	
+	
+}
