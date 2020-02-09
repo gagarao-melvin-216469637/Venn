731829f905263ded49698a0b26ab0ae7b6e65bd4
zhoudoris13 <zhoudoris13@gmail.com>
2020-02-09 12:44:37 -0500

    Add circle

--- VennFx/src/application/Main.java
+++ VennFx/src/application/Main.java
@@ -3,7 +3,14 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.animation.*;
@@ -11,9 +18,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+
+			
+			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(true);
 			primaryStage.show();
@@ -22,7 +33,6 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
