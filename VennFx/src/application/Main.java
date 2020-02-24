107881e27b8f09e690c24c2ff67b409016eb3206
gagarao-melvin-216469637 <melving@my.yorku.ca>
2020-02-23 19:08:10 -0500

    Re-orienting the generators

--- VennFx/src/application/Main.java
+++ VennFx/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -15,25 +15,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.animation.*;
+
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.getIcons().add(new Image("icon.png"));
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			primaryStage.setTitle("Venn Diagram Generator");
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("Sample.fxml"));
 
-			
-			Scene scene = new Scene(root,900,800);
+			Scene scene = new Scene(root, 900, 800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(true);
 			primaryStage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
