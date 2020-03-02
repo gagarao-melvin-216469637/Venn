59e24f127bd3145de1c7cee5b0449ce2f1508154
Hassan Mukhtar <hassan.muk@hotmail.com>
2020-03-02 11:49:41 -0500

    commit

--- Venn/src/main/java/Venn/Main.java
+++ Venn/src/main/java/Venn/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		//Hope you're having a great day
 		System.out.println("Lab 3");
-		System.out.println("Lab 3 part 2");
 		int a = 5;
 		int b = 2;
 	}
