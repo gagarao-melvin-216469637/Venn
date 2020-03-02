7a69db1775fc7a02eb7b6cfbea0444fed71e98b9
gagarao-melvin-216469637 <melving@my.yorku.ca>
2020-03-02 12:32:41 -0500

    Prevented some quick errors with the Counter

--- Venn/src/main/java/Venn/Main.java
+++ Venn/src/main/java/Venn/Main.java
@@ -3,10 +3,10 @@ package Venn;
 public class Main {
 
     public static void main(String[] args) {
-        Counter c = new Counter();
-        c.increment();
-        System.out.println(c.getValue());
-
+//        Counter c = new Counter();
+//        c.increment();
+//        System.out.println(c.getValue());
+    	System.out.println("ok");
     }
 
 }
